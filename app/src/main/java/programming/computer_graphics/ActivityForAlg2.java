7643f7e2987b1b227bc1a318e843e769bde8ec6d
marusencka <mmmmmarusencka@yandex.by>
2017-03-04 00:42:43 +0300

    реализован алгоритм Брезенхема рисования отрезка

--- app/src/main/java/programming/computer_graphics/ActivityForAlg2.java
+++ app/src/main/java/programming/computer_graphics/ActivityForAlg2.java
@@ -10,13 +10,18 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.signum;
+
 public class ActivityForAlg2 extends AppCompatActivity implements View.OnTouchListener{
     class DrawView extends View {
         Paint p;
         PointF p1, p2;
+        int count;
 
         public DrawView(Context context) {
             super(context);
+            count=0;
             p = new Paint();
             p1=new PointF();
             p2=new PointF();
@@ -24,15 +29,57 @@ public class ActivityForAlg2 extends AppCompatActivity implements View.OnTouchLi
         public void setP1(float x, float y){
             p1.x=x;
             p1.y=y;
+            count++;
         }
         public void setP2(float x, float y){
             p2.x=x;
             p2.y=y;
+            count++;
         }
+
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.drawColor(Color.WHITE);
-            canvas.drawLine(p1.x,p1.y,p2.x,p2.y,p);
+            p.setColor(Color.BLACK);
+            p.setStrokeWidth(1);
+            if(count==2) {
+                float x=p1.x;
+                float y=p1.y;
+                float dx=abs(p2.x-p1.x);
+                float dy=abs(p2.y-p1.y);
+                float s1=signum(p2.x-p1.x);
+                float s2=signum(p2.y-p1.y);
+                boolean change=false;
+                if(dy>dx){
+                    change=true;
+                    float temp=dx;
+                    dx=dy;
+                    dy=temp;
+                }
+                float sf=2*dy-dx;
+                float sf1=2*dy;
+                float sf2=2*dx;
+                for(float i=0; i<=dx;i++){
+                    canvas.drawPoint(x,y,p);
+                    while(sf>=0){
+                        if(change){
+                            x+=s1;
+                        }
+                        else{
+                            y+=s2;
+                        }
+                        sf-=sf2;
+                    }
+                    if(change){
+                        y+=s2;
+                    }
+                    else{
+                        x+=s1;
+                    }
+                    sf+=sf1;
+                }
+                count=0;
+            }
         }
     }
     private DrawView view;
